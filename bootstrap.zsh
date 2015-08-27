3e0a5eae55dea80c0a88a2b929b42355c6717268
Pete Schaffner <pschaffner@google.com>
2015-08-27 18:05:22 +0200

    Create new bootstrap and bring back zsh config

---
+++ bootstrap.zsh
@@ -0,0 +1,83 @@
+#!/bin/zsh
+
+#########################
+# Setup Homebrew and Cask
+#########################
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew install ag caskroom/cask/brew-cask cmake fasd git git-extras hub n vim
+brew cask install karabiner seil
+
+
+#########################
+# Setup Vim
+#########################
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+
+##############
+# Setup Prezto
+##############
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+chsh -s /bin/zsh
+
+
+###################################
+# Create symbolic links to dotfiles
+###################################
+
+dotfiles=(~/.dotfiles/{agignore,git*,jshintrc,netrc,tern-config,vim,z*})
+
+for dotfile in $dotfiles; do
+	ln -fs "$dotfile" ~/".${dotfile:t}"
+done
+
+
+#########################
+# Link system keybindings
+#########################
+
+keyindingsDir=~/Library/KeyBindings
+
+mkdir -p $keyindingsDir
+ln -fs ~/.dotfiles/DefaultKeyBinding.dict $keyindingsDir
+
+
+###################
+# Set app shortcuts
+###################
+
+# Key:
+# control => ^
+# option => ~
+# command => @
+# shift => $
+# Nested->Menu Item => \033Nested\033Menu Item
+
+# Sketch
+defaults write com.bohemiancoding.sketch3 NSUserKeyEquivalents '{
+    "Hide Colors" = "@$c";
+    "Show Colors" = "@$c";
+    "Flatten Selection to Bitmap" = "@$f";
+    "Scale..." = "@^s";
+    "Round to Nearest Pixel Edge" = "@~r";
+    "Top" = "@^k";
+    "Right" = "@^l";
+    "Bottom" = "@^j";
+    "Left" = "@^h";
+    "\033Arrange\033Align Objects\033Vertically" = "@^-";
+    "\033Arrange\033Align Objects\033Horizontally" = "@^\\";
+    "\033Arrange\033Distribute Objects\033Vertically" = "$@^-";
+    "\033Arrange\033Distribute Objects\033Horizontally" = "$@^\\";
+}'
